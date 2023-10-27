135c15d506871e11aece9ec2218952a6874f44a6
Umar <muhammadshafqatali3@gmail.com>
2023-10-27 23:51:54 +0500

    dsdsd

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -2,12 +2,15 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar.js";
 import Footer from "./components/Footer.js";
+import CalculatorPage from "./Pages/CalculatorPage";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
-      <Routes>{/* <Route path="/" element={<Home />} /> */}</Routes>
+      <Routes>
+        <Route path="/" element={<CalculatorPage />} />
+      </Routes>
       <Footer />
     </BrowserRouter>
   );
