e1b3d3a643128a0e6d2c7b289cfeee210642c723
Umar <muhammadshafqatali3@gmail.com>
2023-10-27 23:45:42 +0500

    dsdsd

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar.js";
+import Footer from "./components/Footer.js";
 
 function App() {
-  return <div className="App">apppp</div>;
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>{/* <Route path="/" element={<Home />} /> */}</Routes>
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default App;
