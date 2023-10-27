73af302cce947b8b9b995f68973b5c3a3d83d6c8
Umar <muhammadshafqatali3@gmail.com>
2023-10-27 23:39:23 +0500

    dsdsd

---
+++ client-side/src/components/Bike.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Bike = () => {
+  return <div>Bike</div>;
+};
+
+export default Bike;
---
+++ client-side/src/components/Secondary.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Secondary = () => {
+  return <div>Secondary</div>;
+};
+
+export default Secondary;
