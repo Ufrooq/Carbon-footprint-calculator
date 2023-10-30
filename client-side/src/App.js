1477dab34210da652423fd9339a75d22a5e8df0e
Umar <muhammadshafqatali3@gmail.com>
2023-10-30 18:29:50 +0500

    dsdsd

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -3,13 +3,14 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar.js";
 import Footer from "./components/Footer.js";
 import CalculatorPage from "./Pages/CalculatorPage";
+import HomePage from "./Pages/HomePage";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<CalculatorPage />} />
+        <Route path="/" element={<HomePage />} />
       </Routes>
       <Footer />
     </BrowserRouter>
---
+++ client-side/src/Pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Home page</h1>
+      <button>Calculate your CARBIN foot print</button>
+    </div>
+  );
+};
+
+export default HomePage;
